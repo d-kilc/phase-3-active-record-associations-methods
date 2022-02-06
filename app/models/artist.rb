6692c9368e38e05d12e3f2555a4010e8fb11aed8
Daniel <dkilcullen25@gmail.com>
2022-02-06 17:27:27 -0600

    commit tree f347a44f0f52d577f3314c7768504c15d9656d6a

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,17 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    Song.where("artist_id = ?",self.id).first.genre
   end
 
   def song_count
     #return the number of songs associated with the artist
+    Song.where("artist_id = ?", self.id).count
+
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    Artist.find(self.id).genres.count
   end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,16 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    Song.where("genre_id = ?", self.id).count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    Genre.find(self.id).artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    Artist.pluck :name 
   end
 end
